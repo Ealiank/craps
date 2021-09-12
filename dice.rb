55b93d1ccd5c95c1431b1e9a557fb68a3fc6aa79
Ealiank <ealiank@icloud.com>
2021-09-12 11:02:28 -0400

    Adding an array and history

--- dice.rb
+++ dice.rb
@@ -3,19 +3,21 @@
 # This class is for die
 class Dice
   attr_reader :sides
-  attr_accessor :number_of_dice, :color
+  attr_accessor :number_of_dice, :color, :history
 
   def initialize(sides = 6, number_of_dice = 1, color = 'white')
     @sides = sides
     @number_of_dice = number_of_dice
     @color = color
+    @history = []
   end
 
   def roll
-    total = 0
+    rolls = []
     number_of_dice.times do
-      total += rand(1..sides)
+      rolls << rand(1..sides)
     end
-    total
+    history << rolls
+    rolls
   end
 end
