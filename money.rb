c2c77b08182313788ca24894e986fab6e56de1db
Ealiank <ealiank@icloud.com>
2021-10-09 11:07:50 -0400

    Changing money to cents instead of dollars

--- money.rb
+++ money.rb
@@ -5,23 +5,30 @@ end
 
 # this class is about money.
 class Money
-  attr_accessor :balance, :currency
+  attr_accessor :currency
+  attr_reader :balance_in_cents
 
   def initialize(balance, currency = 'USD')
-    @balance = balance
+    @balance_in_cents = balance * 100
     @currency = currency
   end
 
+  def balance
+    balance_in_cents / 100
+  end
+
   def deposit(amount)
-    self.balance = balance + amount
+    self.balance_in_cents = balance_in_cents + amount * 100
+    balance
   end
 
   def withdraw(amount)
-    if amount > balance
+    if amount * 100 > balance_in_cents
       raise(WithdrawError,
-            "You dont have enough money in your bank account. The maximum you can withdraw is #{currency} #{balance}")
+            "You dont have enough money in your account. The max you can withdraw is #{currency} #{balance_in_cents}")
     end
 
-    self.balance = balance - amount
+    self.balance_in_cents = balance_in_cents - amount * 100
+    balance
   end
 end
